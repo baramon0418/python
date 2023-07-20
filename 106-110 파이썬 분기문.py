82ae27f95f808cad9261ed194a94351db51d1851
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2023-07-20 16:36:20 +0900

    Add files via upload

---
+++ 106-110 파이썬 분기문.py
@@ -0,0 +1,27 @@
+if 4 < 3:
+    print("Hello World")
+
+    
+if 4 < 3:
+    print("Hello World.")
+else:
+    print("Hi, there.")
+
+    
+if True :
+    print ("1")
+    print ("2")
+else :
+    print("3")
+print("4")
+
+
+if True :
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else :
+    print("4")
+print("5")
