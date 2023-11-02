3a5398e6fed34cd8c2ede95bc6b791a2b28dbe9f
baramon0418 <108270144+baramon0418@users.noreply.github.com>
2023-11-02 14:42:14 +0900

    Add files via upload

---
+++ 219.py
@@ -0,0 +1,6 @@
+def print_arithmetic_operation(a, b):
+    print(a, "+", b, "=", a + b)
+    print(a, "-", b, "=", a - b)
+    print(a, "*", b, "=", a * b)
+    print(a, "/", b, "=", a / b)
+print_arithmetic_operation(3, 4)
